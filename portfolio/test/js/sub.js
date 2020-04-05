3425b1cd33f0c607610fe8581a2bfe8dd5334a40
seojiyeon <jiyeon9061@gmail.com>
2020-04-05 18:09:33 +0900

    Update sub.js

--- portfolio/test/js/sub.js
+++ portfolio/test/js/sub.js
@@ -7,4 +7,28 @@ $(document).ready(function(){
         var dtc = $('div.detial_cnt');
         $('div.detial_cnt').eq(listIdx).addClass('on');
     });
+    
+    // F12 버튼 방지
+    $(document).ready(function(){
+        $(document).bind('keydown',function(e){
+            if ( e.keyCode == 123 /* F12 */) {
+                e.preventDefault();
+                e.returnValue = false;
+            }
+        });
+    });
+    
+    // 우측 클릭 방지
+    document.onmousedown=disableclick;
+    status="Right click is not available.";
+    
+    function disableclick(event){
+        if (event.button==2) {
+            alert(status);
+            return false;
+        }
+    }
+
+
+출처: https://server-engineer.tistory.com/563 [HelloWorld]
 })

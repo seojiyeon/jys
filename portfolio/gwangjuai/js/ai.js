cc3074af6173618127efe3670e526e0882da857f
seojiyeon <jiyeon9061@gmail.com>
2020-11-29 03:15:21 +0900

    Add files via upload

---
+++ portfolio/gwangjuai/js/ai.js
@@ -0,0 +1,21 @@
+document.onmousedown=disableclick;
+status="Right click is not available.";
+    
+function disableclick(event){
+    if (event.button==2) {
+        alert(status);
+        return false;
+    }
+}
+// 개발자 도구(F12) 방지
+$(function(){
+    $(document).bind('keydown',function(e){
+        if ( e.keyCode == 123 /* F12 */) {
+            e.preventDefault();
+            e.returnValue = false;
+        }
+    });
+});
+
+
+출처: https://baejangho.com/entry/prevent-mouse-right-click [호짱의 개발 블로그]

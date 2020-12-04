a97edcd60a750973f321069a5ef53bc404210cc7
seojiyeon <jiyeon9061@gmail.com>
2020-12-04 16:37:09 +0900

    Add files via upload

---
+++ js/ai.js
@@ -0,0 +1,19 @@
+// document.onmousedown=disableclick;
+// status="Right click is not available.";
+    
+// function disableclick(event){
+//     if (event.button==2) {
+//         alert(status);
+//         return false;
+//     }
+// }
+// // 개발자 도구(F12) 방지
+// $(function(){
+//     $(document).bind('keydown',function(e){
+//         if ( e.keyCode == 123 /* F12 */) {
+//             e.preventDefault();
+//             e.returnValue = false;
+//         }
+//     });
+// });
+
